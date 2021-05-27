dd584cb140985de964a2724cd268d9faf7d56960
Aatreyee <70950558+Aatreyee@users.noreply.github.com>
2021-05-27 22:52:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { StyleSheet, Text, View ,TextInputs,Touchable} from 'react-native';
+import SignupLogin from './screens/SignupLogin';
+
+
+export default function App() {
+  return (
+    
+        <SignupLogin/>
+   
+  );
+}
+
+
